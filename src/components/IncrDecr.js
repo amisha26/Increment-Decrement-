8d2cb544d0cc1d97c7f6fd122647b714401e17d3
Amisha Tiwari <amisha26>
2021-05-07 00:46:15 +0530

    final commit

--- src/components/IncrDecr.js
+++ src/components/IncrDecr.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Button} from "react-bootstrap";
 
 const IncrDecr = () => {
-    state = {
-        count:0
-    };
-    Increment = () => {
-        this.setState({count : this.state.count + 1});
+    const [count,setCount] = useState(0) //initial value define in usestate, count takes initial value, setCount takes updated value
+
+    const Incr = () =>{
+        setCount(count+1)
+    }
+
+    const Decr = () =>{
+        if (count>0){
+        setCount(count-1)
+        }
+        else{
+            alert("Decrement cannot be done")
+        }
     }
     return (
-        <div>
-        <button onClick= {this.Increment} style = {this.styles} className = "btn btn-secondary btn-sm">Increment</button>
+        <div style={{margin:"50px 0 0 0"}}>
+        <h1 style={{margin:"0 0 28px 160px"}}>{count}</h1>
+        <Button onClick= {Incr} variant="success" style={{margin: "0 8px 0 0" }}>Increment</Button>
+        <Button onClick= {Decr} variant="danger" style={{margin: "0 8px 0 0" }}>Decrement</Button>
+        <Button onClick= {() => {setCount(0)}} variant ="primary">Clear</Button>
         </div>
     )
 };
