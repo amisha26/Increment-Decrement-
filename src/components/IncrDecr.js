508a51aa76896f09855a469ae85d7efdc3eb6f7c
amisha26 <amisha8299@gmail.com>
2021-09-13 10:11:00 +0530

    error found

--- src/components/IncrDecr.js
+++ src/components/IncrDecr.js
@@ -1,29 +1,41 @@
-import React,{useState} from 'react';
-import {Button} from "react-bootstrap";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const IncrDecr = () => {
-    const [count,setCount] = useState(0) //initial value define in usestate, count takes initial value, setCount takes updated value
+  //initial value define in usestate, count takes initial value, setCount takes updated value
+  const [count, setCount] = useState(0);
 
-    const Incr = () =>{
-        setCount(count+1)
-    }
+  const Incr = () => {
+    setCount(count + 1);
+  };
 
-    const Decr = () =>{
-        if (count>0){
-        setCount(count-1)
-        }
-        else{
-            alert("Decrement cannot be done")
-        }
+  const Decr = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("Decrement cannot be done");
     }
-    return (
-        <div style={{margin:"50px 0 0 0"}}>
-        <h1 style={{margin:"0 0 28px 160px"}}>{count}</h1>
-        <Button onClick= {Incr} variant="success" style={{margin: "0 8px 0 0" }}>Increment</Button>
-        <Button onClick= {Decr} variant="danger" style={{margin: "0 8px 0 0" }}>Decrement</Button>
-        <Button onClick= {() => {setCount(0)}} variant ="primary">Clear</Button>
-        </div>
-    )
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <Button onClick={Incr} variant="success">
+        Increment
+      </Button>
+      <Button onClick={Decr} variant="danger">
+        Decrement
+      </Button>
+      <Button
+        onClick={() => {
+          setCount(0);
+        }}
+        variant="primary"
+      >
+        Clear
+      </Button>
+    </div>
+  );
 };
 
 export default IncrDecr;
